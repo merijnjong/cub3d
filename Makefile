01b51d16f2ae9272e55b0fade1c4f62e510a4a83
SaltyChickenLeg <merijnjong1@gmail.com>
2025-04-09 13:47:40 +0200

    setup part II

---
+++ Makefile
@@ -0,0 +1,38 @@
+NAME    := cub3d
+CFLAGS  := -g -Wextra -Wall -Werror
+LIBFT   := ./libft
+MLX     := ./MLX42
+BUILD   := $(MLX)/build
+
+INCS    := -I./incs -I$(LIBFT)/incs -I$(MLX)/include
+LIBS    := $(LIBFT)/libft.a $(BUILD)/libmlx42.a -ldl -lglfw -lm -pthread
+SRCS    := $(shell find ./srcs -name "*.c")
+OBJS    := ${SRCS:.c=.o}
+
+all: $(BUILD)/libmlx42.a libft $(NAME)
+
+$(BUILD)/libmlx42.a:
+	@cmake -B $(BUILD) -S $(MLX) -DGLFW_USE_WAYLAND=OFF
+	@cmake --build $(BUILD)
+
+libft:
+	@make -C $(LIBFT)
+
+%.o: %.c
+	@$(CC) $(CFLAGS) -c $< -o $@ $(INCS) && printf "Compiling: $(notdir $<)\n"
+
+$(NAME): $(OBJS)
+	@$(CC) $(OBJS) $(LIBS) -o $(NAME)
+
+clean:
+	@rm -rf $(OBJS)
+	@make clean -C $(LIBFT)
+	@rm -rf $(BUILD)
+
+fclean: clean
+	@rm -f $(NAME)
+	@make fclean -C $(LIBFT)
+
+re: fclean all
+
+.PHONY: all clean fclean re libft
---
+++ incs/cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 13:23:18 by mjong             #+#    #+#             */
+/*   Updated: 2025/04/09 13:35:47 by mjong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <sys/time.h>
+# include "../incs/libft.h"
+# include "MLX42/MLX42.h"
+
+void	ft_write(char *str);
+
+#endif
