333fe327f0dbe0a18215fcb92ac41443b2172626
Devlin Kros <dkros@f1r6s12.codam.nl>
2025-04-16 18:18:59 +0200

    16 april

--- incs/cub3d.h
+++ incs/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dkros <dkros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:23:18 by mjong             #+#    #+#             */
-/*   Updated: 2025/04/16 15:23:53 by mjong            ###   ########.fr       */
+/*   Updated: 2025/04/16 17:51:24 by dkros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@
 	\nCorrect input:./cub3d maps/map.cub\n\033[0m"
 # define MAP_ERROR "\033[1;31mError, invalid map\
 	\nCorrect map format: maps/map.cub\n\033[0m"
+# define SCREEN_WIDTH 1920
+# define SCREEN_HEIGHT 1080
+# define BLOCK_SIZE 50
+
+typedef struct s_line_data_t
+{
+	int	x1;
+	int	x2;
+	int	y1;
+	int	y2;
+	int	dy;
+}	t_line_data;
 
 typedef struct t_data
 {
@@ -35,14 +47,20 @@ typedef struct t_data
 	char		**two_d_map;
 	mlx_t		*mlx;
 	mlx_image_t	*roof;
-	mlx_image_t	*floor;
+	mlx_image_t *img;
+	t_line_data c;
+	mlx_image_t *floor;
 	mlx_image_t	*north;
 	mlx_image_t	*east;
 	mlx_image_t	*south;
 	mlx_image_t	*west;
 }	t_game;
 
-void	ft_hooks(mlx_key_data_t keydata, t_game *game);
+void		ft_hooks(mlx_key_data_t keydata, t_game *game);
+// void	prep_draw_line(t_game *data);
+// void	draw_line(t_game *data, int dx, int sx, int sy);
+mlx_image_t *draw_square(t_game *data, int x, int y, int size, int color);
+void		my_pixel_put(mlx_image_t *img, int x, int y, int color);
 
 //srcs/utils.c
 void	print_dbl_ptr(char **ptr);
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dkros <dkros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:22:43 by mjong             #+#    #+#             */
-/*   Updated: 2025/04/16 15:21:11 by mjong            ###   ########.fr       */
+/*   Updated: 2025/04/16 18:17:04 by dkros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	init(t_game *game)
 	game->map_width = 0;
 	game->map_height = 0;
 	game->two_d_map = NULL;
-	game->mlx = mlx_init(1920, 1080, "cub3d", true);
+	game->mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d", false);
 	game->roof = NULL;
 	game->floor = NULL;
 	game->north = NULL;
@@ -52,6 +52,90 @@ void	init(t_game *game)
 	game->south = NULL;
 	game->west = NULL;
 }
+
+// void	prep_draw_line(t_game *data)
+// {
+// 	int	dx;
+// 	int	sx;
+// 	int	sy;
+
+// 	dx = abs(data->c.x2 - data->c.x1);
+// 	data->c.dy = abs(data->c.y2 - data->c.y1);
+// 	if (data->c.x1 < data->c.x2)
+// 		sx = 1;
+// 	else
+// 		sx = -1;
+// 	if (data->c.y1 < data->c.y2)
+// 		sy = 1;
+// 	else
+// 		sy = -1;
+// 	draw_line(data, dx, sx, sy);
+// 	return ;
+// }
+
+void	my_pixel_put(mlx_image_t *img, int x, int y, int color)
+{
+	if (x >= 0 && x < (int)img->height && y >= 0 && y < (int)img->width)
+	{
+		mlx_put_pixel(img, x, y, color);
+	}
+	return;
+}
+
+void	draw_line(t_game *data, int dx, int sx, int sy)
+{
+	int	e2;
+	int	err;
+
+	if (dx > data->c.dy)
+		err = dx / 2;
+	else
+		err = -(data->c.dy) / 2;
+	while (1)
+	{
+		my_pixel_put(data, (data->c.x1 + 100), (data->c.y1 + 75));
+		if (data->c.x1 == data->c.x2 && data->c.y1 == data->c.y2)
+			break ;
+		e2 = err;
+		if (e2 > -dx)
+		{
+			err -= data->c.dy;
+			data->c.x1 += sx;
+		}
+		if (e2 < data->c.dy)
+		{
+			err += dx;
+			data->c.y1 += sy;
+		}
+	}
+	return ;
+}
+
+mlx_image_t *draw_square(t_game *data, int x, int y, int size, int color)
+{
+	int i;
+	int j;
+
+
+	(void)color;
+	data->img = mlx_new_image(data->mlx, size, size);
+	if (!data->img)
+	return (NULL);
+	i = 0;
+	while (i < (size - 1))
+	{
+		j = 0;
+		while (j < (size - 1))
+		{
+			my_pixel_put(data->img, j, i, color);
+			j++;
+		}
+		i++;
+	}
+	mlx_image_to_window(data->mlx, data->img, x, y);
+	return (data->img);
+}
+
 
 int	main(int argc, char **argv)
 {
@@ -64,6 +148,32 @@ int	main(int argc, char **argv)
 	}
 	file_to_map(&game, argv[1]);
 	init(&game);
-	mlx_key_hook(game.mlx, (void *)&ft_hooks, &game);
+
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		j = 0;
+		while (j < 5)
+		{
+			if (test[(i * 5) + j] && test[(i * 5) + j] == 1)
+				draw_square(&game, (j * BLOCK_SIZE), (i * BLOCK_SIZE), BLOCK_SIZE, 0x000000FF);
+			else if (test[(i * 5) + j] && test[(i * 5) + j] == 2)
+				draw_square(&game, (j * BLOCK_SIZE), (i * BLOCK_SIZE), BLOCK_SIZE, 0XFF0000FF);
+			else
+				draw_square(&game, (j * BLOCK_SIZE), (i * BLOCK_SIZE), BLOCK_SIZE, 0XFFFFFFFF);
+			j++;
+		}
+		i++;
+	}
+
+	if (game.mlx)
+	{
+		mlx_key_hook(game.mlx, (void *)&ft_hooks, &game);
+		mlx_loop(game.mlx);
+	}
 	return (0);
 }
